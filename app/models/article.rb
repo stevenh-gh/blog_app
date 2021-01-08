a69a3d5e21cec5478f859be1559cd84befd9ad30
steven <steven.h@live.com>
2021-01-07 17:45:42 -0800

    finished up to 8.2 also added a link_to link to homepage (root path) in article's show template

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+  has_many :comments
   validates :title, presence: true # declares title must be present
   validates :body, presence: true, length: { minimum: 10 } # declares body to be present, a min length of 10 chars
 end
